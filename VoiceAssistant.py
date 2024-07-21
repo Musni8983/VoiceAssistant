ffc83709aa6137d2780d871485404d203c3787ad
Musni8983 <murathmusni@gmail.com>
2024-07-21 12:57:43 +0530

    Update all the libraries

---
+++ VoiceAssistant.py
@@ -0,0 +1,16 @@
+import datetime
+import wikipedia
+import webbrowser
+import os
+import pyttsx3
+import wikipediaapi
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import ttk
+import pyjokes
+from ecapture import ecapture as ec
+import time
+import wolframalpha
+import random
+import smtplib
+from googlesearch import search
